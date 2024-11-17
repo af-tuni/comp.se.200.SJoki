7d666fb802cad5db3bac7efa5b116c22ffa3fb10
af-tuni <aleksi.frimodig@gmail.com>
2024-11-17 10:54:44 +0200

    unit tests for add.js

--- __tests__/add.test.js
+++ __tests__/add.test.js
@@ -1,5 +1,30 @@
 import add from "../src/add";
 
-test('sums parameters correctly', () => {
+test('sums positive numbers correctly', () => {
     expect(add(1,2)).toBe(3);
 })
+test('sums negative numbers correctly', () => {
+    expect(add(-1,-3)).toBe(-4)
+})
+test('sums decimals correctly', () => {
+    expect(add(1,1.5)).toBe(2.5)
+})
+test('sums string correctly', () => {
+    expect(add("a","b")).toBe("ab")
+})
+test('when other number is missing should return the one that is given', () => {
+    expect(add(1)).toBe(1)
+})
+test('without any parameters should return zero', () => {
+    expect(add()).toBe(0)
+})
+test('with big numbers should still work', () => {
+    expect(add(10**6, 10**6)).toBe(10**6+10**6)
+})
+test('with infinity should return infinity', () => {
+    expect(add(1, Infinity)).toBe(Infinity)
+})
+test('array in parameter should return NaN', () => {
+    expect(add([1,2], 3)).toBe(NaN)
+})
+
